22ef57f4d35ecea63aaf6cd222d86f7441bbe13e
Kyaw Khant <kyawkhant@Kyaws-Air.lan>
2025-09-29 12:11:54 -0700

    excel can read and printout all sheet(not selective)//1st commit

---
+++ src/main/java/org/example/Excel.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Excel
+{
+    public static void main( String[] args )
+    {
+        dataman tester = new dataman();
+        tester.readSheets();
+
+    }
+}
---
+++ src/main/java/org/example/dataman.java
@@ -0,0 +1,44 @@
+package org.example;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class dataman {
+    private static final String test = "src/main/java/org/example/test.xlsx";
+    public static void readSheets(){
+        try {
+            FileInputStream file = new FileInputStream(new File(test));
+            Workbook workbook = new XSSFWorkbook(file);
+            DataFormatter formatter = new DataFormatter();
+            Iterator<Sheet> sheetIterator = workbook.sheetIterator();
+            while(sheetIterator.hasNext()) {
+                Sheet sheet = sheetIterator.next();
+                System.out.println("Sheet Name: " + sheet.getSheetName());
+                System.out.println("Row Count: " + sheet.getRow(0).getLastCellNum());
+                System.out.println("-----------------");
+                Iterator<Row> rowIterator = sheet.rowIterator();
+                while(rowIterator.hasNext()){
+                   Row row = rowIterator.next();
+                   Iterator<Cell> cellIterator = row.cellIterator();
+                   while(cellIterator.hasNext()){
+                       Cell cell = cellIterator.next();
+
+                       String cellValue = formatter.formatCellValue(cell);
+                       System.out.print(cellValue+" \t");
+                   }
+                   System.out.println();
+                }
+                System.out.println("-----------------");
+            }
+            workbook.close();
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
